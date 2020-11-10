677a270618442865e54394cab4905231d9a723fe
Brady Fillmore <bfillmore24@biomedscienceacademy.org>
2020-11-10 17:01:19 +0000

    I wrote out all of my story and variables

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Brady Fillmore
+Period/Core: 5th
 
 
 """
+name = input("What is your name? ")
+print(f"Welcome to your very own madlib {name}!")
+animal_noun1= input("Animal: ")
+noun2= input("Noun: ")
+past_tense_verb1= input("Past tense verb: ")
+adjetive1=  input("Color: ")
+adjetive2= input("Adjetive: ")
+clothing_noun= input("Article of clothing: ")
+occupation_noun= input("Occupation: ")
+city_noun= input("City: ")
+city_noun2= input("Different City: ")
+adjetive3= input("Adjetive: ")
+adjetive4= input("Adjetive: ")
+
+print("-------------------------------------------\n")
+print (f"The {animal_noun1} {past_tense_verb1} by the {noun2}!\nIt was {adjetive1}!")
+print (f"Even more {adjetive2}, it was wearing a {clothing_noun}!")
+print (f"The {occupation_noun} saw it all happen!")
+print (f"He instantly called his friend in {city_noun} to tell him what happened.\n He didn't beleive him!")
+print ("He told him \"Come to "+ city_noun2 + ", I'll show you\"")
+print (f"When he got there, the {animal_noun1} wasn't there.")
+print (f"He called the baker a {adjetive3} {adjetive4} liar!")
